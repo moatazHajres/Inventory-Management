32008a02d307b6bd059480bf0a8f57f92c41d188
Ghassan Aljaafri <ghassan.aljaafri@gmail.com>
2021-05-19 22:17:14 +0200

    Edit Entities

--- Inventory Management/Entities/BaseEntity.cs
+++ Inventory Management/Entities/BaseEntity.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace Inventory_Management.Entities
 {
-    class BaseEntity
+    abstract class BaseEntity
     {
-        public static string tableName;
-
         public int Id { get; set; }
     }
 }
