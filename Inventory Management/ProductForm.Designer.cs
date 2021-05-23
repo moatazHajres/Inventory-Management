70e2f32dd4c0be7b283291429640f81a574ad7c3
Ghassan Aljaafri <ghassan.aljaafri@gmail.com>
2021-05-23 13:29:07 +0200

    Decimal Places 3

--- Inventory Management/ProductForm.Designer.cs
+++ Inventory Management/ProductForm.Designer.cs
@@ -56,36 +56,40 @@ namespace Inventory_Management
             | System.Windows.Forms.AnchorStyles.Right)));
             this.ProductsDgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.ProductsDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.ProductsDgv.Location = new System.Drawing.Point(387, 88);
+            this.ProductsDgv.Location = new System.Drawing.Point(258, 60);
+            this.ProductsDgv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ProductsDgv.Name = "ProductsDgv";
             this.ProductsDgv.RowHeadersWidth = 62;
             this.ProductsDgv.RowTemplate.Height = 29;
-            this.ProductsDgv.Size = new System.Drawing.Size(726, 346);
+            this.ProductsDgv.Size = new System.Drawing.Size(484, 237);
             this.ProductsDgv.TabIndex = 11;
             this.ProductsDgv.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ProductsDgv_CellDoubleClick);
             this.ProductsDgv.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.ProductsDgv_RowHeaderMouseClick);
             // 
             // ProductNameTxt
             // 
-            this.ProductNameTxt.Location = new System.Drawing.Point(12, 39);
+            this.ProductNameTxt.Location = new System.Drawing.Point(8, 27);
+            this.ProductNameTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ProductNameTxt.Multiline = true;
             this.ProductNameTxt.Name = "ProductNameTxt";
-            this.ProductNameTxt.Size = new System.Drawing.Size(367, 42);
+            this.ProductNameTxt.Size = new System.Drawing.Size(246, 30);
             this.ProductNameTxt.TabIndex = 1;
             // 
             // ProductBarcodeTxt
             // 
-            this.ProductBarcodeTxt.Location = new System.Drawing.Point(12, 110);
+            this.ProductBarcodeTxt.Location = new System.Drawing.Point(8, 75);
+            this.ProductBarcodeTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ProductBarcodeTxt.Multiline = true;
             this.ProductBarcodeTxt.Name = "ProductBarcodeTxt";
-            this.ProductBarcodeTxt.Size = new System.Drawing.Size(367, 42);
+            this.ProductBarcodeTxt.Size = new System.Drawing.Size(246, 30);
             this.ProductBarcodeTxt.TabIndex = 2;
             // 
             // AddProductBtn
             // 
-            this.AddProductBtn.Location = new System.Drawing.Point(12, 227);
+            this.AddProductBtn.Location = new System.Drawing.Point(8, 155);
+            this.AddProductBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AddProductBtn.Name = "AddProductBtn";
-            this.AddProductBtn.Size = new System.Drawing.Size(369, 42);
+            this.AddProductBtn.Size = new System.Drawing.Size(246, 29);
             this.AddProductBtn.TabIndex = 4;
             this.AddProductBtn.Text = "Save Product";
             this.AddProductBtn.UseVisualStyleBackColor = true;
@@ -94,18 +98,20 @@ namespace Inventory_Management
             // ProductNameLbl
             // 
             this.ProductNameLbl.AutoSize = true;
-            this.ProductNameLbl.Location = new System.Drawing.Point(8, 18);
+            this.ProductNameLbl.Location = new System.Drawing.Point(5, 12);
+            this.ProductNameLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ProductNameLbl.Name = "ProductNameLbl";
-            this.ProductNameLbl.Size = new System.Drawing.Size(59, 19);
+            this.ProductNameLbl.Size = new System.Drawing.Size(39, 13);
             this.ProductNameLbl.TabIndex = 4;
             this.ProductNameLbl.Text = "*Name";
             // 
             // ProductBarcodeLbl
             // 
             this.ProductBarcodeLbl.AutoSize = true;
-            this.ProductBarcodeLbl.Location = new System.Drawing.Point(8, 88);
+            this.ProductBarcodeLbl.Location = new System.Drawing.Point(5, 60);
+            this.ProductBarcodeLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ProductBarcodeLbl.Name = "ProductBarcodeLbl";
-            this.ProductBarcodeLbl.Size = new System.Drawing.Size(74, 19);
+            this.ProductBarcodeLbl.Size = new System.Drawing.Size(51, 13);
             this.ProductBarcodeLbl.TabIndex = 5;
             this.ProductBarcodeLbl.Text = "*Barcode";
             // 
@@ -113,19 +119,21 @@ namespace Inventory_Management
             // 
             this.SearchProductsTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.SearchProductsTxt.Location = new System.Drawing.Point(387, 39);
+            this.SearchProductsTxt.Location = new System.Drawing.Point(258, 27);
+            this.SearchProductsTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SearchProductsTxt.Multiline = true;
             this.SearchProductsTxt.Name = "SearchProductsTxt";
-            this.SearchProductsTxt.Size = new System.Drawing.Size(412, 42);
+            this.SearchProductsTxt.Size = new System.Drawing.Size(276, 30);
             this.SearchProductsTxt.TabIndex = 8;
             this.SearchProductsTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SearchProductsTxt_KeyDown);
             // 
             // ReloadProductsBtn
             // 
             this.ReloadProductsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.ReloadProductsBtn.Location = new System.Drawing.Point(963, 39);
+            this.ReloadProductsBtn.Location = new System.Drawing.Point(642, 27);
+            this.ReloadProductsBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ReloadProductsBtn.Name = "ReloadProductsBtn";
-            this.ReloadProductsBtn.Size = new System.Drawing.Size(150, 44);
+            this.ReloadProductsBtn.Size = new System.Drawing.Size(100, 30);
             this.ReloadProductsBtn.TabIndex = 10;
             this.ReloadProductsBtn.Text = "Reload Products";
             this.ReloadProductsBtn.UseVisualStyleBackColor = true;
@@ -134,9 +142,10 @@ namespace Inventory_Management
             // SearchBtn
             // 
             this.SearchBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.SearchBtn.Location = new System.Drawing.Point(807, 39);
+            this.SearchBtn.Location = new System.Drawing.Point(538, 27);
+            this.SearchBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SearchBtn.Name = "SearchBtn";
-            this.SearchBtn.Size = new System.Drawing.Size(150, 44);
+            this.SearchBtn.Size = new System.Drawing.Size(100, 30);
             this.SearchBtn.TabIndex = 9;
             this.SearchBtn.Text = "Search";
             this.SearchBtn.UseVisualStyleBackColor = true;
@@ -145,17 +154,19 @@ namespace Inventory_Management
             // ProductSearchLbl
             // 
             this.ProductSearchLbl.AutoSize = true;
-            this.ProductSearchLbl.Location = new System.Drawing.Point(382, 18);
+            this.ProductSearchLbl.Location = new System.Drawing.Point(255, 12);
+            this.ProductSearchLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ProductSearchLbl.Name = "ProductSearchLbl";
-            this.ProductSearchLbl.Size = new System.Drawing.Size(56, 19);
+            this.ProductSearchLbl.Size = new System.Drawing.Size(41, 13);
             this.ProductSearchLbl.TabIndex = 9;
             this.ProductSearchLbl.Text = "Search";
             // 
             // EditProductBtn
             // 
-            this.EditProductBtn.Location = new System.Drawing.Point(12, 275);
+            this.EditProductBtn.Location = new System.Drawing.Point(8, 188);
+            this.EditProductBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.EditProductBtn.Name = "EditProductBtn";
-            this.EditProductBtn.Size = new System.Drawing.Size(369, 42);
+            this.EditProductBtn.Size = new System.Drawing.Size(246, 29);
             this.EditProductBtn.TabIndex = 5;
             this.EditProductBtn.Text = "Update Product";
             this.EditProductBtn.UseVisualStyleBackColor = true;
@@ -163,9 +174,10 @@ namespace Inventory_Management
             // 
             // DeleteProductBtn
             // 
-            this.DeleteProductBtn.Location = new System.Drawing.Point(12, 323);
+            this.DeleteProductBtn.Location = new System.Drawing.Point(8, 221);
+            this.DeleteProductBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DeleteProductBtn.Name = "DeleteProductBtn";
-            this.DeleteProductBtn.Size = new System.Drawing.Size(369, 42);
+            this.DeleteProductBtn.Size = new System.Drawing.Size(246, 29);
             this.DeleteProductBtn.TabIndex = 6;
             this.DeleteProductBtn.Text = "Delete Product";
             this.DeleteProductBtn.UseVisualStyleBackColor = true;
@@ -174,17 +186,19 @@ namespace Inventory_Management
             // ProductPriceLbl
             // 
             this.ProductPriceLbl.AutoSize = true;
-            this.ProductPriceLbl.Location = new System.Drawing.Point(8, 156);
+            this.ProductPriceLbl.Location = new System.Drawing.Point(5, 107);
+            this.ProductPriceLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ProductPriceLbl.Name = "ProductPriceLbl";
-            this.ProductPriceLbl.Size = new System.Drawing.Size(52, 19);
+            this.ProductPriceLbl.Size = new System.Drawing.Size(35, 13);
             this.ProductPriceLbl.TabIndex = 13;
             this.ProductPriceLbl.Text = "*Price";
             // 
             // ResetFieldsBtn
             // 
-            this.ResetFieldsBtn.Location = new System.Drawing.Point(12, 371);
+            this.ResetFieldsBtn.Location = new System.Drawing.Point(8, 254);
+            this.ResetFieldsBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ResetFieldsBtn.Name = "ResetFieldsBtn";
-            this.ResetFieldsBtn.Size = new System.Drawing.Size(369, 42);
+            this.ResetFieldsBtn.Size = new System.Drawing.Size(246, 29);
             this.ResetFieldsBtn.TabIndex = 7;
             this.ResetFieldsBtn.Text = "Reset Fields";
             this.ResetFieldsBtn.UseVisualStyleBackColor = true;
@@ -192,21 +206,23 @@ namespace Inventory_Management
             // 
             // ProductPriceInput
             // 
+            this.ProductPriceInput.DecimalPlaces = 3;
             this.ProductPriceInput.Increment = new decimal(new int[] {
             25,
             0,
             0,
             131072});
-            this.ProductPriceInput.Location = new System.Drawing.Point(12, 178);
+            this.ProductPriceInput.Location = new System.Drawing.Point(8, 122);
+            this.ProductPriceInput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ProductPriceInput.Name = "ProductPriceInput";
-            this.ProductPriceInput.Size = new System.Drawing.Size(368, 27);
+            this.ProductPriceInput.Size = new System.Drawing.Size(245, 20);
             this.ProductPriceInput.TabIndex = 3;
             // 
             // ProductForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1130, 450);
+            this.ClientSize = new System.Drawing.Size(753, 308);
             this.Controls.Add(this.ProductPriceInput);
             this.Controls.Add(this.ResetFieldsBtn);
             this.Controls.Add(this.ProductPriceLbl);
@@ -222,6 +238,7 @@ namespace Inventory_Management
             this.Controls.Add(this.ProductBarcodeTxt);
             this.Controls.Add(this.ProductNameTxt);
             this.Controls.Add(this.ProductsDgv);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ProductForm";
             this.Text = "Product Form";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ProductForm_FormClosed);
